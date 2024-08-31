c200c99bbbb7489abf3360269fd36e06e22470ff
Andrew Allen <andrew@Andrews-MacBook-Air.local>
2024-08-31 03:25:31 +0100

    increase default zoom

--- app/components/NoGoAreaMap.tsx
+++ app/components/NoGoAreaMap.tsx
@@ -28,7 +28,7 @@ export default function NoGoAreaMap({
   return (
     <div className="max-w-6xl mx-auto p-4">
       <div className="mb-4 rounded-lg overflow-hidden shadow-lg">
-        <MapContainer center={userLocation} zoom={13} className="h-[600px] w-full">
+        <MapContainer center={userLocation} zoom={15} className="h-[600px] w-full">
           <MapEvents onClick={onMapClick} />
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
