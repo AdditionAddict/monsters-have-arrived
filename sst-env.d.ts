1b9511254770cc1ec94f965f2a0ae21ebf2b6af8
Andrew Allen <andrew@Andrews-MacBook-Air.local>
2024-08-31 03:25:16 +0100

    setup sst

---
+++ sst-env.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+import "sst"
+declare module "sst" {
+  export interface Resource {
+    "SurvivalHubWebApp": {
+      "type": "sst.aws.Remix"
+      "url": string
+    }
+  }
+}
+export {}
